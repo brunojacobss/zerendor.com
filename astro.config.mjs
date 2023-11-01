23637fa7ccd7e6bae1d12b312c78545c76fc0cc8
Bruno Jacobs <brunojacobsschulz@gmail.com>
2023-11-01 12:17:55 -0500

    Add netlify specifics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   devOptions: {
-    hostname: "0.0.0.0",
+    hostname: "0.0.0.0"
   },
+  output: "server",
+  adapter: netlify()
 });
